9867e016c4876d195334404459bec0ce1f9935e2
Otis Parsons <ocparsons@gmail.com>
2020-12-30 14:54:20 +0000

    Country Page added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,6 +4,7 @@ import styles from "./NavBar.module.scss";
 const NavBar = () => {
   return (
     <nav className={styles.navBar}>
+      
       <header className={styles.header}>COVID 19 TRACKER</header>
       <span className={styles.github}>GITHUB 1 GITHUB 2</span>
     </nav>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,32 +1,21 @@
 import React, { useState } from "react";
 import styles from "./SearchBar.module.scss";
 
-// const SearchBar = () => {
-//   return (
-//     <div className={styles.searchbar}>
-//       <p>SearchBar works</p>
-//     </div>
-//   );
-// };
 
-const SearchBar = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { placeholder, updateSearchText } = props;
-  const input = isOpen ? (
-    <input
-      type="text"
-      placeholder={placeholder}
-      autoFocus={true}
-      onInput={(e) => updateSearchText(e.target.value)}
-    />
-  ) : null;
+const SearchBar = ({keyword,setKeyword}) => {
 
   return (
+ 
     <div className={styles.search}>
-      {input}
-      <span className={styles.fa} onClick={() => setIsOpen(!isOpen)}>
-        <p>WORKS</p>
-      </span>
+      <input
+        key="random1"
+        className={styles.input}
+        type="search"
+        placeholder="search for country"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        
+      />
     </div>
   );
 };
